96e62a7699061950f86403aafad1d4472de72c18
tn3rb <brent@eventespresso.com>
2024-05-10 12:57:24 -0700

    updates from cafe repo

--- wp-api-remove-auth-header.php
+++ wp-api-remove-auth-header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: WP API Remove Authorization Header
  * Description: Plugin which simulates how CGI and Fast-CGI remove the "Authorization" HTTP header on requests. This helps test if your WP API authentication method can handle that header being removed.
@@ -7,6 +8,6 @@
  * Version: 0.1
  */
 
-//ahem-hem, let me begin: remove the PHP auth variables.
-//if the header weren't present, these wouldn't have been set.
+// ahem-hem, let me begin: remove the PHP auth variables.
+// if the header weren't present, these wouldn't have been set.
 unset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'], $_SERVER['HTTP_AUTHORIZATION']);
